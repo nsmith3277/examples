f500de9b053d5ac4c9362468386fc489bb74a945
Anthony Deeter <aed27@zips.uakron.edu>
2015-09-14 11:34:52 -0400

    changed to working version

--- stacks_and_queues/stacks/main.cpp
+++ stacks_and_queues/stacks/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
     size = DEFAULT_STACK_SIZE;
   
   AStack<int> *as = new AStack<int>(size);
-  for (int i = 0; i < size+1; ++i)
+  for (int i = 0; i < size; ++i)
     as->push(i);
   
   for (int i = 0; i < size; ++i)
