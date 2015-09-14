020514b7bf85fe203be3c50e29883a06352688c1
Anthony Deeter <aed27@zips.uakron.edu>
2015-09-14 12:24:55 -0400

    Added documentation

--- stacks_and_queues/stacks/AStack.hpp
+++ stacks_and_queues/stacks/AStack.hpp
@@ -4,15 +4,23 @@
 #include <cassert>
 #include "Stack.hpp"
 
+// we need a default size in case one is not given
 const int DEFAULT_SIZE = 10;
 
 template <class T>
 class AStack : public Stack<T> {
 private:
+  // the max size the array can be
   int max_size;
+
+  // the index of the current top of the stack
   int top = 0;
+
+  // the actual list in memory of our items
   T* list; // the actual list of elements
+
 public:
+  // construct with the default size if one is not given
   AStack(int size=DEFAULT_SIZE);
   ~AStack();
   
@@ -25,12 +33,17 @@ public:
 
 template <class T>
 AStack<T>::AStack(int size) {
+  // set the max size
   max_size = size;
+
+  // create the list in memory
   list = new T[size];
 }
 
+
 template <class T>
 AStack<T>::~AStack() {
+  // delete the list from memory
   delete [] list;
 }  
 
@@ -38,18 +51,26 @@ template <class T>
 void AStack<T>::push(const T& t) {
   //check if stack is full
   assert(top < max_size);
+
+  //insert the item into the array and move the top index
   list[top++] = t;
 }
 
 template <class T>
 T AStack<T>::pop() {
+  // make sure the list is not empty
   assert(top > 0);
+
+  // move the top pointer and return the item from the list
   return list[--top];
 }
 
 template <class T>
 const T& AStack<T>::peek() const {
+  // make sure the list is not empty
   assert(top > 0);
+
+  // return the top item data without remove it
   return list[top - 1];
 }
 
--- stacks_and_queues/stacks/Stack.hpp
+++ stacks_and_queues/stacks/Stack.hpp
@@ -1,6 +1,11 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+/*
+  The base class for the Stack ADT
+  This will be inherited for both array and linked list versions
+ */
+
 template <class T>
 class Stack
 {
@@ -8,12 +13,22 @@ private:
   
 public:
   Stack() {}
+  // always virtualize destructors when inheritance will occur
   virtual ~Stack() {}
 
+  // used to push a new item on that stack
   virtual void push(const T&)=0;
+
+  // pop an item off the stack and return its data
   virtual T pop()=0;
+
+  // look at the data from the item at the top of the stack without removing it
   virtual const T& peek() const =0;
+
+  // remove all items from the stack
   virtual void clear()=0;
+
+  // the number of items on the stack
   virtual int size()=0;
 };
 
